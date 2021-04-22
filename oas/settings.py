178173ea938b3e7292f6c7954f0dce4638446535
Rizwan Salik <mrizwansalik@gmail.com>
2021-04-22 15:10:50 +0500

    url patren changed

--- oas/settings.py
+++ oas/settings.py
@@ -100,8 +100,8 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:3000",
+    "http://127.0.0.1:8000",
     "http://localhost:3001",
-    "http://127.0.0.1:3000",
 ]
 
 TEMPLATES = [
