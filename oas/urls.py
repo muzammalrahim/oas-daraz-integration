50a86942610123f20762fe406423d163d71134e8
Rizwan Salik <mrizwansalik@gmail.com>
2021-04-23 09:19:18 +0500

    swagger api url added

--- oas/urls.py
+++ oas/urls.py
@@ -38,12 +38,12 @@ schema_view = get_schema_view(
 )
 urlpatterns = [
     path('_admin/', admin.site.urls),
-    # url(r'^swagger(?P<format>\.json|\.yaml)$',
-    #     schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    # url(r'^$', schema_view.with_ui('swagger', cache_timeout=0),
-    #     name='schema-swagger-ui'),
-    # url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
-    #     name='schema-redoc'),
+    url(r'^swagger(?P<format>\.json|\.yaml)$',
+        schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^$', schema_view.with_ui('swagger', cache_timeout=0),
+        name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
+        name='schema-redoc'),
     path('api/v1/user_dashboard', views.user_dashboard),
     path('api/v1/', include('api.urls')),
     path('', include('user.urls')),
