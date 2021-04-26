73bf00d900bcb8d39af822ba656b1e5c4e2ef0d4
Rizwan Salik <mrizwansalik@gmail.com>
2021-04-26 10:11:00 +0500

    detail page changes

--- frontend/user/src/component/Footer.js
+++ frontend/user/src/component/Footer.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {Row, Col, Container } from 'react-bootstrap';
 import {Link} from 'react-router-dom'
 import { FaCcVisa, FaCcPaypal,FaCcAmazonPay } from "react-icons/fa"
